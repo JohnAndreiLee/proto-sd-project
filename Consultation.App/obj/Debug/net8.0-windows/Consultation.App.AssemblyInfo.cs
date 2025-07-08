80569039d236967a36a41b12b130dc8f638c423f
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-07-08 23:22:17 +0800

    Delited TestViews folder

--- Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
+++ Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e085d9455762c6dd0263ce877111ca7acbc5d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2001629cca7497918954f2b9c355d512d441fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
