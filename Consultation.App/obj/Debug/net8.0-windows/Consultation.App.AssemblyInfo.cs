31543a7e9d42c22cdb04427ad2c111f7b80dfe6d
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-07-13 02:58:32 +0800

    MainView button highlight

--- Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
+++ Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b16b0a7e64407ff51de4f858fc38db81a74dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a3fcef4d1cef007dccc6e67cc696953706283a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
