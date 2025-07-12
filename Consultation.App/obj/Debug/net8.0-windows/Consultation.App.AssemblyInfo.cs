6fcd5bd347e8840ff5f28401054f762afcefdad2
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-07-12 20:31:12 +0800

    conversion from Forms to UserControl

--- Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
+++ Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2444ebd0e8c9cc6120f23c6d34f787065d1c828c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a3c1a70459f1682583b238816947f292c56475")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
